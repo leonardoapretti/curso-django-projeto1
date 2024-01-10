907d6043a206531b8a4adc7cc25fa83f1fcad74d
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-01-10 15:42:40 -0300

    Configurando o diretório de arquivos estáticos globais da aplicação
    Foi criada a variável STATICFILES_DIRS em settings.py que recebe uma lista de caminhos para os diretórios que contenham esses arquivos estáticos globais

--- projeto/settings.py
+++ projeto/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'UTC'
 
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
