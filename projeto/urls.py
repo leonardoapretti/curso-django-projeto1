5161e4ba0e31d9d1b935031277ea86fcb357d75b
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-01-10 09:39:13 -0300

    Separação das urlS em arquivos específicos

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,15 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def my_view(request):
-    return HttpResponse('String')
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),
+    # inclui as urls específicas do app para deixar o código melhor
+    path('', include('recipes.urls'))
 ]
