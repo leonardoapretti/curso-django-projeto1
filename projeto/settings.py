433b64a07ab13a2165870f707517772dc96fba9b
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-01-10 10:19:16 -0300

    Corrigindo a rota para contatos e criando o template contato.html

--- projeto/settings.py
+++ projeto/settings.py
@@ -59,7 +59,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / 'base_templates',
-            BASE_DIR / 'recipes' / 'temp_templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
