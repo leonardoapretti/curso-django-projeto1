c952244e54eac52ec4f63a20abef9070c9ad196f
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-01-14 12:11:02 -0300

    Levantando exceção 404 caso não hajam receitas na categoria

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import Http404
 
 
 def home(request):
@@ -14,9 +15,13 @@ def home(request):
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found')
+
     context = {
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category'
+        'title': f'{recipes.first().category.name} - Category',
     }
     return render(request, 'recipes/pages/category.html', context=context)
 
