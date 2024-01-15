9e5db836f337fb2e888ae9d66b21e9257574dd43
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-01-14 21:52:50 -0300

    TEstando se as urls estão referenciando as funções de view corretas
    1) Testar as Urls; 2) Testar as funções de view

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from recipes import views
 
 # pip install pytest-watch + comando ptw = serve para rodar os testes em loop. Sempre que salvar o arquivo ele atualiza o teste
 
@@ -17,3 +18,17 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id_recipe': 4})
         self.assertEqual(url, '/recipes/4/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertTrue(view.func is views.home)
+
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 4}))
+        self.assertTrue(view.func is views.category)
+
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id_recipe': 4}))
+        self.assertTrue(view.func is views.recipe)
