aad020e8efa65f2e9a2847cadbbfa543dd231a7e
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-01-14 23:28:13 -0300

    Separdo dos testes no diretório tests

---
+++ recipes/tests/test_recipe_urls.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RecipeURLsTest(TestCase):
+    # Ser super descritivo no nome dos testes
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 4})
+        self.assertEqual(url, '/recipes/category/4/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id_recipe': 4})
+        self.assertEqual(url, '/recipes/4/')
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -5,30 +5,15 @@ from recipes import views
 # pip install pytest-watch + comando ptw = serve para rodar os testes em loop. Sempre que salvar o arquivo ele atualiza o teste
 
 
-class RecipeURLsTest(TestCase):
-    # Ser super descritivo no nome dos testes
-    def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 4})
-        self.assertEqual(url, '/recipes/category/4/')
-
-    def test_recipe_detail_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id_recipe': 4})
-        self.assertEqual(url, '/recipes/4/')
-
-
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertTrue(view.func is views.home)
 
-    def test_recipe_home_view_function_is_correct(self):
+    def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 4}))
         self.assertTrue(view.func is views.category)
 
-    def test_recipe_home_view_function_is_correct(self):
+    def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id_recipe': 4}))
         self.assertTrue(view.func is views.recipe)
