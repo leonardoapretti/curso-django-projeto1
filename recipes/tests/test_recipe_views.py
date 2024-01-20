26a0d0f56aebb0af5a8e7726dc7d67fb132b554b
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-01-20 19:04:12 -0300

    Corrigindo o teste test_recipe_home_template_shows_no_recipes_found_if_no_recipes por que o método setUp(self) cria uma receita para todos os testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -7,24 +7,7 @@ from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest(TestCase):
-    def test_recipe_home_view_function_is_correct(self):
-        view = resolve(reverse('recipes:home'))
-        self.assertTrue(view.func is views.home)
-
-    def test_recipe_home_view_returns_status_code_200_ok(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_recipe_home_view_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertTemplateUsed(response, 'recipes/pages/home.html')
-
-    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertIn('Nenhuma receita para mostrar',
-                      response.content.decode('utf-8'))
-
-    def test_recipe_home_template_loads_recipes(self):
+    def setUp(self) -> None:
         category = Category.objects.create(name='Category')
         author = User.objects.create_user(
             first_name='user',
@@ -47,6 +30,30 @@ class RecipeViewsTest(TestCase):
             preparation_steps_is_html=False,
             is_published=True,
         )
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertTrue(view.func is views.home)
+
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        Recipe.objects.get(pk=1).delete()
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn('Nenhuma receita para mostrar',
+                      response.content.decode('utf-8'))
+
+    def test_recipe_home_template_loads_recipes(self):
 
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
