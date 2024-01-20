203054e3ab19173242954949b3f0e9f37b0bff13
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-01-20 19:46:54 -0300

    Finalizando a classe geradora de receitas

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -6,9 +6,6 @@ from .test_recipe_base import RecipeTestBase
 
 class RecipeViewsTest(RecipeTestBase):
 
-    def tearDown(self) -> None:
-        return super().tearDown()
-
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertTrue(view.func is views.home)
@@ -22,22 +19,20 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
-        Recipe.objects.get(pk=1).delete()
         response = self.client.get(reverse('recipes:home'))
         self.assertIn('Nenhuma receita para mostrar',
                       response.content.decode('utf-8'))
 
     def test_recipe_home_template_loads_recipes(self):
-
+        # need a recipe for this test
+        self.make_recipe()
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
-        self.assertIn('Recipe Title', content)
-        self.assertIn('10 Minutos', content)
-        self.assertIn('5 Porções', content)
-        self.assertEqual(len(response_context_recipes), 1)
 
-        pass
+        # check if one recipe exists
+        self.assertIn('Recipe Title', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 4}))
