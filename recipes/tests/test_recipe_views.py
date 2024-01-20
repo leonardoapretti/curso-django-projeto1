6bbe46402413a93d3457e612fd84352f1cd09f66
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-01-20 19:10:44 -0300

    Criando uma classe para fazer as receitas da base de dados temporária - CONTINUA

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,36 +1,10 @@
-from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-from recipes.models import Category, Recipe, User
-
+from .test_recipe_base import RecipeTestBase
 # pip install pytest-watch + comando ptw = serve para rodar os testes em loop. Sempre que salvar o arquivo ele atualiza o teste
 
 
-class RecipeViewsTest(TestCase):
-    def setUp(self) -> None:
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
-        return super().setUp()
+class RecipeViewsTest(RecipeTestBase):
 
     def tearDown(self) -> None:
         return super().tearDown()
