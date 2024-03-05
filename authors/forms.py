09a958e97e7e64dbecf8752d511817a04e412783
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-03-05 17:10:50 -0300

    139. Utilizando o atributo validators do próprio campo para validação de dados

--- authors/forms.py
+++ authors/forms.py
@@ -2,6 +2,7 @@ from typing import Any
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+import re
 
 
 def add_attr(field, attr_name, attr_new_val):
@@ -11,6 +12,18 @@ def add_attr(field, attr_name, attr_new_val):
 
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
 
 
 class RegisterForm(forms.ModelForm):
@@ -34,7 +47,8 @@ class RegisterForm(forms.ModelForm):
         help_text=(
             'Password must have at least one upper case letter, '
             'one lowercase letter and one number.'
-        )
+        ),
+        validators=[strong_password]
     )
     password2 = forms.CharField(
         required=True,
