d2727b6a713b33f16fed5f5f17851c1e7f38e498
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-04-02 12:40:37 -0300

    204. ListView: criando uma base para Home, Category e Search de recipes

---
+++ recipes/views/__init__.py
@@ -0,0 +1,2 @@
+from .all import *
+from .recipe_list_view_base import *
---
+++ recipes/views/recipe_list_view_base.py
@@ -0,0 +1,36 @@
+from django.views.generic import ListView
+from utils.pagination import make_pagination
+from recipes.models import Recipe
+import os
+
+PER_PAGE = os.environ.get('PER_PAGE', 6)
+
+# CRIA A CLASSE BASE DE RECIPES
+
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+
+    # MANIPULA A QUERY_SET (POR PADRÃO O DJANDO RECUPERA TODOS OS OBJETOS DO MODEL)
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        filtered_qs = qs.filter(
+            is_published=True
+        )
+        return filtered_qs
+
+    # MANIPULA O CONTEXTO (SERVE PARA ATUALIZAR O CONTEXTO DA APLICAÇÃO)
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request,
+            ctx.get('recipes'),
+            PER_PAGE,
+        )
+        ctx.update(
+            {'recipes': page_obj, 'pagination_range': pagination_range}
+        )
+        return ctx
