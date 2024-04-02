4207ba676d3d2da923fc54a31aac94d658120dd3
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-04-02 10:01:47 -0300

    200. Criando o método post e reorganizando a class based view

--- authors/urls.py
+++ authors/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
          views.dashboard_recipe_delete, name='dashboard_recipe_delete'),
     path('dashboard/recipe/<int:id>/edit/',
          views.DashboardRecipe.as_view(), name='dashboard_recipe_edit'),
+    #
 ]
--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -8,38 +8,49 @@ from django.urls import reverse
 
 
 class DashboardRecipe(View):
+
+    def get_recipe(self, id):
+        recipe = None
+        if id:
+            recipe = Recipe.objects.filter(
+                is_published=False,
+                author=self.request.user,
+                pk=id,
+            ).first()
+            if not recipe:
+                raise Http404()
+        return recipe
+
+    def render_recipe(self, context):
+        return render(self.request, 'authors/pages/dashboard_recipe.html', context)
+
     def get(self, request, id):
-        recipe = Recipe.objects.filter(
-            is_published=False,
-            author=request.user,
-            pk=id,
-        ).first()
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(
+            instance=recipe,
+        )
+        context = {
+            'form': form,
+        }
+        return self.render_recipe(context)
 
-        if not recipe:
-            raise Http404()
-
+    def post(self, request, id):
+        recipe = self.get_recipe(id)
         form = AuthorRecipeForm(
             request.POST or None,
             files=request.FILES or None,
             instance=recipe,
         )
-
         if form.is_valid():
             recipe = form.save(commit=False)
-
             recipe.author = request.user
             recipe.preparation_steps_is_html = False
             recipe.is_published = False
-
             recipe.save()
-
             messages.success(request, 'Your recipe has been updated!')
             return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
-
         context = {
             'recipe': recipe,
             'form': form,
         }
-
-        return render(request, 'authors/pages/dashboard_recipe.html', context)
-    ...
+        return self.render_recipe(context)
