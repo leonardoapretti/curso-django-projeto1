1b2004b870172a66646e478a2147f510b190e0d5
Leonardo dos Anjos Pretti <leonardoapretti@gmail.com>
2024-04-02 17:36:04 -0300

    207. Search ListView

--- recipes/views/recipe_list_view_base.py
+++ recipes/views/recipe_list_view_base.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView
 from utils.pagination import make_pagination
 from recipes.models import Recipe
+from django.db.models import Q
 import os
 
 PER_PAGE = os.environ.get('PER_PAGE', 6)
@@ -55,5 +56,29 @@ class RecipeListViewCategory(RecipeListViewBase):
         title = ctx.get('object_list')[0].category.name
         ctx.update({
             'title': title,
+        })
+        return ctx
+
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        self.search_term = self.request.GET.get('q', '').strip()
+        filtered_qs = qs.filter(
+            Q(
+                Q(title__icontains=self.search_term) |
+                Q(description__icontains=self.search_term)
+            ),
+        )
+        return filtered_qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        ctx.update({
+            'title': f'Search for "{self.search_term}"',
+            'search_term': self.search_term,
+            'additional_url_query': f'&q={self.search_term}',
         })
         return ctx
